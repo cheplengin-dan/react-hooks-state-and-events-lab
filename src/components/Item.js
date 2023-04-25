2b189603bb8d12c3480aeb932d4699c6d22f5bf1
duncan kiptoo <cheplenginduncan@gmail.com>
2023-04-25 20:53:33 +0300

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  const addToCart = () => {
+    setInCart(true);
+    };
+
+  const removeFromCart = () => {
+    setInCart(false);
+    };
+
+  const buttonAction = inCart ? removeFromCart : addToCart;
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart";
+  const liClassName = inCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={buttonAction}>
+      {buttonText}
+      </button>
     </li>
   );
 }
